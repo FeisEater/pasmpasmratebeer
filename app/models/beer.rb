cfc3300212dc4b236af98390e6fccceeb31248d8
Pavel <feiseater@ubuntu.(none)>
2015-01-21 15:37:19 +0200

    rating average

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		x = 0
+		ratings.each do |r|
+			x += r.score
+		end
+		x /= ratings.count
+		x
+	end
 end
