0940cb17587863f6be1234a29838b1d9ef1c99d0
Pavel <feiseater@ubuntu.(none)>
2015-01-31 16:56:45 +0200

    fixed bug with beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,13 @@
 class Beer < ActiveRecord::Base
-	include RatingAverage
+  include RatingAverage
 
-	belongs_to :brewery
-	has_many :ratings
+  belongs_to :brewery
+  has_many :ratings, dependent: :destroy
+  has_many :raters, -> { uniq }, through: :ratings, source: :user
 
-	def to_s
-		"#{name}, from #{brewery.name}"	
-	end
+  validates :name, presence: true
+
+  def to_s
+    "#{name}, from #{brewery.name}"	
+  end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,18 @@
 class Brewery < ActiveRecord::Base
-	include RatingAverage
+  include RatingAverage
 
-	has_many :beers
-	has_many :ratings, through: :beers
+  has_many :beers
+  has_many :ratings, through: :beers
+
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+                                   only_integer: true }
+  validate :not_from_future
+
+  def not_from_future
+    if year > Time.now.year
+      errors[:year] << "Morty! They found me! I don't know how but they found me!"
+    end
+  end
+
 end
